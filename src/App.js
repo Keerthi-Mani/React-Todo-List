3f7c57a57348a2fac792ea6b4bc7b8ac50eafe33
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-13 09:42:16 -0500

    import note component in app file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Note from "./components/Note";
 import "./App.css";
 class App extends Component {
   constructor(props) {
@@ -16,12 +17,27 @@ class App extends Component {
     if (event.key === "Enter") {
     }
   };
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.aplice(index, 1);
+    this.setState({ notes: notesArr });
+  }
+
   render() {
+    let notes = this.state.notes.map((val, key) => {
+      return (
+        <Note key={key} text={val} deleteMethod={() => this.deleteNote(key)} />
+      );
+    });
+
     return (
       <div className="container">
         <div className="header">React Todo Application</div>
 
-        <div className="btn">+</div>
+        <div className="btn" onClick={this.addNote.bind(this)}>
+          +
+        </div>
 
         <input
           type="text"
